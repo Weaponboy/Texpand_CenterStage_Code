0f761f9ee72c9593cf0fcca379e064280489abda
James4444444 <jschepp11@gmail.com>
2023-10-19 19:54:48 +0200

    Added hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code_Under_Development/Teleop/hardwareControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Code_Under_Development/Teleop/hardwareControl.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Code_Under_Development.Teleop;
 
 import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -11,7 +12,7 @@ import static org.firstinspires.ftc.teamcode.Code_Under_Development.Constants_an
 import static org.firstinspires.ftc.teamcode.Code_Under_Development.Constants_and_Setpoints.Constants.slide_i;
 import static org.firstinspires.ftc.teamcode.Code_Under_Development.Constants_and_Setpoints.Constants.slide_p;
 
-public class hardwareControl {
+public class hardwareControl extends OpMode {
 
     public DcMotorEx LF;
     public DcMotorEx LB;
@@ -28,17 +29,22 @@ public class hardwareControl {
     public static double pivot_p = 0.004, pivot_i = 0, pivot_d = 0.0001;
 
 
+    HardwareMap harwareMap;
 
-   HardwareMap harwareMap;
 
-    public void init(HardwareMap Hmap){
+    public void init(HardwareMap Hmap) {
 
-        harwareMap = Hmap;
 
-        LF = Hmap.get(DcMotorEx.class, "LF");
-        LB = Hmap.get(DcMotorEx.class, "LB");
-        RF = Hmap.get(DcMotorEx.class, "RF");
-        RB = Hmap.get(DcMotorEx.class, "RB");
+    }
+
+    @Override
+    public void init() {
+
+
+        LF = hardwareMap.get(DcMotorEx.class, "LF");
+        LB = hardwareMap.get(DcMotorEx.class, "LB");
+        RF = harwareMap.get(DcMotorEx.class, "RF");
+        RB = hardwareMap.get(DcMotorEx.class, "RB");
 
         RF.setDirection(DcMotorSimple.Direction.FORWARD);
         LF.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -56,8 +62,8 @@ public class hardwareControl {
         LB.setPower(0);
 
 
-        Left_Slide = Hmap.get(DcMotorEx.class, "Left_Slide");
-        Right_Slide = Hmap.get(DcMotorEx.class, "Right_Slide");
+        Left_Slide = harwareMap.get(DcMotorEx.class, "Left_Slide");
+        Right_Slide = hardwareMap.get(DcMotorEx.class, "Right_Slide");
 
         Right_Slide.setDirection(DcMotorSimple.Direction.REVERSE);
 
@@ -69,8 +75,7 @@ public class hardwareControl {
 
         Slide_Power = new PIDFController(slide_p, slide_i, slide_d, 0);
 
-        Intake = Hmap.get(DcMotorEx.class, "intake");
-
+        Intake = harwareMap.get(DcMotorEx.class, "intake");
 
 
         Intake.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -79,8 +84,44 @@ public class hardwareControl {
 
         Intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+    }
 
+    @Override
+    public void loop() {
+
+        double vertical = -gamepad1.right_stick_y;
+        double horizontal = gamepad1.right_stick_x*1.5;
+        double pivot = gamepad1.left_stick_x;
+
+        RF.setPower((-pivot + (vertical - horizontal)));
+        RB.setPower((1.15)*(-pivot + (vertical + horizontal)));
+        LF.setPower((pivot + (vertical + horizontal)));
+        LB.setPower((1.15)*(pivot + (vertical - horizontal)));
+
+
+        if (gamepad1.a) {
+            Intake.setPower(1);
+
+
+        } else if (gamepad1.a) {
+            Intake.setPower(0);
+        }
+
+        if (gamepad1.b) {
+            Left_Slide.setPower(1);
+
+        } else if (gamepad1.b) {
+            Left_Slide.setPower(0);
+        }
+        if (gamepad1.b) {
+            Right_Slide.setPower(1);
+
+        } else if (gamepad1.b) {
+            Right_Slide.setPower(0);
+
+        }
 
 
     }
 }
+
